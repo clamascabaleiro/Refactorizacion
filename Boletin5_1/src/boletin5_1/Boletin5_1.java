39af05c8f144e911514946b363123d64278a10af
clamascabaleiro <clamascabaleiro@nautilus15.danielcastelao.org>
2018-10-24 12:38:40 +0200

    Sin Terminar

--- Boletin5_1/src/boletin5_1/Boletin5_1.java
+++ Boletin5_1/src/boletin5_1/Boletin5_1.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package boletin5_1;
-
+import java.util.Scanner;
 /**
  *
  * @author clamascabaleiro
@@ -16,6 +16,26 @@ public class Boletin5_1 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+    Scanner ler=new Scanner (System.in);
+    Conta datos = new Conta ();
+    System.out.println("Introduzca cliente:");
+    datos.setNombre(ler.next());
+    System.out.println("Cliente= " + datos.getNombre());
+    System.out.println("Introduzca conta:");
+    datos.setNumeroconta(ler.next());
+    System.out.println("Cliente= " + datos.getNombre());
+    System.out.println("Conta= " +datos.getNumeroconta());
+    System.out.println("Introduzca saldo:");
+    datos.setSaldo(ler.nextDouble());
+    System.out.println("Cliente= " + datos.getNombre());
+    System.out.println("Conta= " +datos.getNumeroconta());
+    System.out.println("Saldo= " +datos.getSaldo());
+    System.out.println("Introduzca cantidad a añadir:");
+    datos.setSaldo(ler.nextDouble());
+    System.out.println("Cliente= " + datos.getNombre());
+    System.out.println("Conta= " +datos.getNumeroconta());
+    System.out.println("Saldo= " +datos.getSaldo());
+    
+    
     }
-
-}
+    }
